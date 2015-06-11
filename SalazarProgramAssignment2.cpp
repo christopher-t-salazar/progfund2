2da903deaa61acfda06bbc41c69a7e97c637b2c8
Christopher Salazar <christopher.salazar.t@gmail.com>
2015-06-11 10:44:58 -0500

    Added Assignment 2

---
+++ SalazarProgramAssignment2.cpp
@@ -0,0 +1,28 @@
+//******************************************************************
+// Programming Assignment 2
+// Programmer: Christopher Salazar
+// Completed : 06/11/2015
+// Status    : Complete
+//
+// This program will help user balance his checkout.
+//******************************************************************
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double balance;
+
+	cout << "****Checkbook Balancing Program****\n\n" << endl;
+	cout << "Enter the beginning balance: ";
+	cin >> balance;
+	cout << "\n~~~~Commands~~~~" << endl;
+	cout << "C - Process a check. " << endl;
+	cout << "D - Process a deposit." << endl;
+	cout << "E - End the program." << endl;
+	cout << "Enter Transaction Type: ";
+	return 0;
+	
+}
