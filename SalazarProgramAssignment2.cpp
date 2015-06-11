0164bf35671f9b1549aa2e691f73482bd983d834
Christopher Salazar <christopher.salazar.t@gmail.com>
2015-06-11 13:36:22 -0500

    Added more lines to code

--- SalazarProgramAssignment2.cpp
+++ SalazarProgramAssignment2.cpp
@@ -13,16 +13,53 @@ using namespace std;
 
 int main()
 {
-	double balance;
+	double balance;  													//User Balance
+	double transAmount;												//Transaction amount
+	double totalCharge; 											//Total Charges
+	const double serviceCharge = 0.25;				//Service Charges
+	char userEntry;  													//User Choice Input
+	const char choice1 = 'C';									//Process Check
+	const char choice2 = 'D';									//Process Deposit
+	const char choice3 = 'E';									//End Program
+
 
 	cout << "****Checkbook Balancing Program****\n\n" << endl;
 	cout << "Enter the beginning balance: ";
 	cin >> balance;
-	cout << "\n~~~~Commands~~~~" << endl;
-	cout << "C - Process a check. " << endl;
-	cout << "D - Process a deposit." << endl;
-	cout << "E - End the program." << endl;
-	cout << "Enter Transaction Type: ";
+
+	while (balance <= 0)
+	{
+		cout <<"Error: Enter Beginning Balance again: ";
+		cin >> balance;
+	}
+
+//Do-While loop to drive menu/selection choices.
+	do
+	{
+		cout << "\n~~~~Commands~~~~" << endl;
+		cout << "C - Process a check. " << endl;
+		cout << "D - Process a deposit." << endl;
+		cout << "E - End the program." << endl;
+		cout << "\nEnter Transaction Type: ";
+		cin >> userEntry;
+
+		while (userEntry != choice1 && userEntry != choice2 && userEntry != choice3)
+		{
+			cout << "Error: Please enter a correct Transaction Type: ";
+			cin >> userEntry;
+		}
+		if (userEntry == choice1)
+		{
+			cout << "Enter transaction amount:";
+			cin >> transAmount;
+			while (transAmount <=0) 
+			{
+				cout << "Error: Please re-enter a valid transaction amount: "
+				cin <<transAmount;
+			}
+			cout << "Processing check for" << transAmount <<endl;
+		}
+	} while(userEntry != choice3);
 	return 0;
-	
+
 }
